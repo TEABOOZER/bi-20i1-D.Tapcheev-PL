248b4557aa306c052e9d9d2edfb5dd579ceba55a
TEABOOZER <72230489+TEABOOZER@users.noreply.github.com>
2020-10-10 10:16:16 +0600

    Add files via upload

---
+++ lab_2/lab_2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <locale>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(NULL));
+    int num = rand() % 100;
+    int a = 0;
+    int num2 = 0;
+    while (num2 != num)
+    {
+        ++a;
+        cout << "Введите число: ";
+        cin >> num2;
+        if (num < num2)
+            cout << "Слишком большое число!" << endl;
+        else if (num > num2)
+            cout << "Слишком маленькое число!" << endl;
+    }
+    cout << endl << "Вы угадали число!" << endl;
+    return 0;
+}
