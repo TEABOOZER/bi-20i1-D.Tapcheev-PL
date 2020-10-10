e9f83a08c14c3fb1686be9d76617a9039e3859d9
TEABOOZER <72230489+TEABOOZER@users.noreply.github.com>
2020-10-10 10:12:21 +0600

    Add files via upload

---
+++ lab_1/lab_1.cpp
@@ -0,0 +1,21 @@
+﻿#include <cstdlib>
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int number;
+    int a = 0;
+    cout << "Введите число: " << endl;
+    cin >> number;
+    while (int b = number % 10)
+    {
+        if (!(b & 1))
+        {
+            ++a;
+        }
+        number /= 10;
+    }
+   cout << "Количество чётных чисел = " << a << endl;
+    return 0;
+}
